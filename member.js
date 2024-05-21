1f72ad9739bc4f0552a8e71f972ffdf5048e8a92
bjur781 <79810883+bjur781@users.noreply.github.com>
2024-05-21 23:40:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,100 @@
+function skillsMember() {
+  return [
+    {
+      name: "React",
+      icon: "fab fa-react",
+      level: "80"
+    },
+    {
+      name: "React Native",
+      icon: "fab fa-react",
+      level: "70"
+    },
+    {
+      name: "Nodejs",
+      icon: "fab fa-node-js",
+      level: "70"
+    },
+    {
+      name: "MongoDB",
+      icon: "fas fa-database",
+      level: "70"
+    },
+    {
+      name: "PostgreSQL",
+      icon: "fas fa-database",
+      level: "60"
+    },
+    {
+      name: "Docker",
+      icon: "fab fa-docker",
+      level: "60"
+    },
+    {
+      name: "Kubernetes",
+      icon: "fab fa-kubernetes",
+      level: "60"
+    },
+    {
+      name: "AWS",
+      icon: "fab fa-aws",
+      level: "60"
+    },
+    {
+      name: "GCP",
+      icon: "fab fa-google",
+      level: "60"
+    },
+    {
+      name: "Git",
+      icon: "fab fa-github",
+      level: "80"
+    },
+    {
+      name: "Jenkins",
+      icon: "fab fa-jenkins",
+      level: "60"
+    },
+    {
+      name: "Jest",
+      icon: "fab fa-js",
+      level: "70"
+    },
+    {
+      name: "Cypress",
+      icon: "fab fa-js",
+      level: "70"
+    },
+    {
+      name: "CircleCI",
+      icon: "fab fa-circle",
+      level: "70"
+    },
+    {
+      name: "Karma",
+      icon: "fab fa-js",
+      level: "60"
+    },
+    {
+      name: "Jasmine",
+      icon: "fab fa-js",
+      level: "60"
+    },
+    {
+      name: "WebPack",
+      icon: "fas fa-cog",
+      level: "70"
+    },
+    {
+      name: "Babel",
+      icon: "fas fa-cogs",
+      level: "70"
+    },
+    {
+      name: "Redux",
+      icon: "fab fa-react",
+      level: "70"
+    },
+    {
+      name: "MobX",
+      icon: "fab
